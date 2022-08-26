2a5952091779a25c52e880b40c494cc0cba372c4
Michael Toriola <michael.toriola@finixio.com>
2022-08-26 13:28:17 +0100

    Github api testing

--- git-test-plugin.php
+++ git-test-plugin.php
@@ -3,12 +3,14 @@
 /**
  * Plugin Name:       Git Test Plugin
  * Description:       A test plugin called test-repo
- * Version:           0.0.1
+ * Version:           0.0.2
  * Author:            Finixio
  * Domain Path:       /languages
  * Text Domain:       git-test-plugin
  * Network:           true
  * GitHub Plugin URI: https://github.com/toriolam/git-test-plugin
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
  */
 
-echo "This is a plugin called git-test-plugin";
+echo "This is a plugin called git-test-plugin v0.0.2";
